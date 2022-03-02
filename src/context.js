8b53f5aec6bba00b57a1fec50d46e430606de764
Sherlok <sergei_malin@yahoo.com>
2022-03-02 10:24:55 -0600

    sidebar-modal

---
+++ src/context.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSideBarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSideBarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
